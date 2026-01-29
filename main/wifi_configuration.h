20e0b03bece9e36f2a8d5b3d64fb5110f3ed1ee1
heavensunshine <36678538+heavensunshine@users.noreply.github.com>
2026-01-30 02:59:14 +0800

    Comment out OTA WiFi configuration
    Comment out the OTA WiFi configuration entry.

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -15,7 +15,7 @@ static struct {
     const char *ssid;
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
-    {.ssid = "OTA", .password = "12345678"},
+    //{.ssid = "OTA", .password = "12345678"},
     {.ssid = "DAP", .password = "12345678"},
     {.ssid = "Xiaomi_3214", .password = "ASD123456"},
     // Add your WAP like this:
